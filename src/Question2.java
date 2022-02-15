6c5a29eba34f51a9f2b80903da336830e52013a2
nolife <voidky@Hotmail.com>
2022-02-15 10:43:25 +0800

    Question2 updated

--- src/Question2.java
+++ src/Question2.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args){
-        int capacity = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the stack capacity : ");
+        int capacity = sc.nextInt();
         StackOfIntegers stack = new StackOfIntegers(capacity);
+
         for (int i = 0; i < capacity; i++) {
             stack.push(i);
         }
